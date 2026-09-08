418dbca4f934c13cff3c53a2b8f2069fa60cc81b
danlakelake <danlakelake@gmail.com>
2026-09-08 14:05:41 -0600

    feat: complete specialties section

--- src/components/sections/Specialties.tsx
+++ src/components/sections/Specialties.tsx
@@ -7,8 +7,36 @@ import orthopedics from '../../assets/specialties/orthopedics.avif';
 import gynecology from '../../assets/specialties/gynecology.avif';
 import ophthalmology from '../../assets/specialties/ophthalmology.avif';
 import internalMedicine from '../../assets/specialties/internal-medicine.avif';
+import { useState } from 'react';
 
 function Specialties() {
+  const categories = [
+    'Todas',
+    'Cardiología',
+    'Neurología',
+    'Pediatría',
+    'Cirugía',
+  ];
+
+  const [selectedCategory, setSelectedCategory] = useState('Todas');
+
+  const specialties = [
+    { name: 'Cardiología', image: cardiology },
+    { name: 'Neurología', image: neurology },
+    { name: 'Pediatría', image: pediatrics },
+    { name: 'Cirugía', image: surgery },
+    { name: 'Cardiología', image: dermatology },
+    { name: 'Neurología', image: orthopedics },
+    { name: 'Pediatría', image: gynecology },
+    { name: 'Neurología', image: ophthalmology },
+    { name: 'Cirugía', image: internalMedicine },
+  ];
+
+  const filteredSpecialties =
+    selectedCategory === 'Todas'
+      ? specialties
+      : specialties.filter((specialty) => specialty.name === selectedCategory);
+
   return (
     <section className="section-padding bg-blue-sections py-20">
       <div className="section-container">
@@ -30,78 +58,27 @@ function Specialties() {
             </p>
             {/* Categorías */}
             <div className="flex flex-wrap gap-5 md:gap-8 items-center py-7 lg:py-0">
-              <button>Todas</button>
-              <button>Cardiología</button>
-              <button>Neurología</button>
-              <button>Pediatría</button>
-              <button>Cirugía</button>
+              {categories.map((category) => (
+                <button
+                  className={`cursor-pointer ${selectedCategory === category ? 'text-primary font-semibold' : ''}`}
+                  onClick={() => setSelectedCategory(category)}
+                >
+                  {category}
+                </button>
+              ))}
             </div>
             {/* Galería */}
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 pt-15">
-            <div>
-              <img
-                src={cardiology}
-                alt="Cardiología"
-                className="w-full h-60 object-cover"
-              />
-            </div>
-            <div>
-              <img
-                src={neurology}
-                alt="Neurología"
-                className="w-full h-60 object-cover"
-              />
-            </div>
-            <div>
-              <img
-                src={pediatrics}
-                alt="Pediatría"
-                className="w-full h-60 object-cover"
-              />
-            </div>
-            <div>
-              <img
-                src={surgery}
-                alt="Cirugía"
-                className="w-full h-60 object-cover"
-              />
-            </div>
-            <div>
-              <img
-                src={dermatology}
-                alt="Dermatología"
-                className="w-full h-60 object-cover"
-              />
-            </div>
-            <div>
-              <img
-                src={orthopedics}
-                alt="Ortopedia"
-                className="w-full h-60 object-cover"
-              />
-            </div>
-            <div>
-              <img
-                src={gynecology}
-                alt="Ginecología"
-                className="w-full h-60 object-cover"
-              />
-            </div>
-            <div>
-              <img
-                src={ophthalmology}
-                alt="Oftalmología"
-                className="w-full h-60 object-cover"
-              />
-            </div>
-            <div>
-              <img
-                src={internalMedicine}
-                alt="Medicina interna"
-                className="w-full h-60 object-cover"
-              />
-            </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-5 pt-5 md:pt-15">
+            {filteredSpecialties.map((specialty) => (
+              <div key={specialty.image} className="animate-fade-in">
+                <img
+                  src={specialty.image}
+                  alt={specialty.name}
+                  className="grid-images"
+                />
+              </div>
+            ))}
           </div>
         </div>
       </div>
