45bfc7512c4f5f09b0f1d31b34b35e2ef1e01d8a
danlakelake <danlakelake@gmail.com>
2026-09-24 18:24:31 -0600

    ft: Fix responsive brand spacing & padding Hero

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -1,6 +1,6 @@
 function Brands() {
   return (
-    <div className="flex justify-between bg-white py-10 text-gray-brands text-lg md:absolute md:left-1/2 md:-translate-x-1/2 md:-bottom-4 md:gap-20 md:rounded-full md:shadow-lg md:px-20">
+    <div className="flex justify-center gap-6 bg-white py-10 text-gray-brands text-lg md:absolute md:left-1/2 md:-translate-x-1/2 md:-bottom-4 md:gap-20 md:rounded-full md:shadow-lg md:px-20">
       <span>colorlib.</span>
       <span>AWWWARDS</span>
       <span>Forbes</span>
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,7 +6,7 @@ function Hero() {
     <section id="home" className="relative scroll-mt-20">
       <div className="hero-container">
         {/* Contenido & CTA */}
-        <div className="w-full md:w-[60%] flex justify-center p-7 px-30 md:px-0">
+        <div className="w-full md:w-[60%] flex justify-center p-7 px-20 md:px-0">
           <div className="text-center md:text-left space-y-4">
             <h1 className="section-title">Medical Network</h1>
             <p>
